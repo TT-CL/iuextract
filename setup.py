f02e78de965cc4e1a8ee5989cdf6628c5fa2db1b
Marcello Gecchele <10898872+CarbonaraJack@users.noreply.github.com>
2024-02-03 12:52:11 +0100

    New pypi version
    Changes to be committed:
    modified:   setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="iuextract",
-    version="0.0.6",
+    version="0.0.7",
     author="Gecchele Marcello",
     author_email="git@gecchele.dev",
     description="Extract Idea Units from strings and files",
