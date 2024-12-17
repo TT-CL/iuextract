83464dce0dbd8a8c52847e5713064ae16f5751e8
Marcello Gecchele <10898872+CarbonaraJack@users.noreply.github.com>
2024-12-17 19:50:49 +0900

    Version bump
    Changes to be committed:
    modified:   pyproject.toml
    modified:   setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="iuextract",
-    version="1.0.5",
+    version="1.0.6",
     author="Gecchele Marcello",
     author_email="git@gecchele.dev",
     description="Rule-based Idea Unit segmentation algorithm for the English language.",
